d5d9cffff765b1e0fbbb18ef9e0afb2f523a511a
proegssilb <proegssilb@gmail.com>
2022-12-02 23:32:38 +0000

    Optimize Day 2

--- src/day2.rs
+++ src/day2.rs
@@ -22,19 +22,20 @@ pub type Round = (GameThrow, GameThrow);
 pub fn input_generator(input: &str) -> Vec<RoundInput> {
     let mut results: Vec<RoundInput> = Vec::new();
     for round_str in input.lines() {
-        let round_bits: Vec<&str> = round_str.split_whitespace().collect();
-        let (opponent_str, self_str) = (round_bits[0], round_bits[1]);
+        let [opponent_str, _, self_str] = round_str.as_bytes() else {
+            panic!("Invalid line as input: {:?}", round_str)
+        };
         let opp_move = match opponent_str {
-            "A" => GameThrow::Rock,
-            "B" => GameThrow::Paper,
-            "C" => GameThrow::Scissors,
-            _ => panic!("Invalid entry found for opponent move.")
+            b'A' => GameThrow::Rock,
+            b'B' => GameThrow::Paper,
+            b'C' => GameThrow::Scissors,
+            _ => panic!("Invalid entry found for opponent move: {:?}", round_str)
         };
         let self_move = match self_str {
-            "X" => SecondColumn::X,
-            "Y" => SecondColumn::Y,
-            "Z" => SecondColumn::Z,
-            _ => panic!("Invalid entry found for second_column.")
+            b'X' => SecondColumn::X,
+            b'Y' => SecondColumn::Y,
+            b'Z' => SecondColumn::Z,
+            _ => panic!("Invalid entry found for second_column: {:?}", round_str)
         };
         results.push((opp_move, self_move));
     }
@@ -42,7 +43,7 @@ pub fn input_generator(input: &str) -> Vec<RoundInput> {
 }
 
 /// Given an opponent's move, what move will cause us to win?
-fn which_wins(act: GameThrow) -> GameThrow {
+const fn which_wins(act: GameThrow) -> GameThrow {
     match act {
         GameThrow::Rock => GameThrow::Paper,
         GameThrow::Paper => GameThrow::Scissors,
@@ -51,7 +52,7 @@ fn which_wins(act: GameThrow) -> GameThrow {
 }
 
 /// Given an opponent's move, what move will cause us to lose?
-fn which_loses(act: GameThrow) -> GameThrow {
+const fn which_loses(act: GameThrow) -> GameThrow {
     match act {
         GameThrow::Rock => GameThrow::Scissors,
         GameThrow::Paper => GameThrow::Rock,
@@ -63,7 +64,7 @@ fn is_win(opp_move: GameThrow, self_move: GameThrow) -> bool {
     self_move == which_wins(opp_move)
 }
 
-fn interpret_round_p1(inr: &RoundInput) -> Round {
+const fn interpret_round_p1(inr: &RoundInput) -> Round {
     let self_move = match inr {
         (_, SecondColumn::X) => GameThrow::Rock,
         (_, SecondColumn::Y) => GameThrow::Paper,
@@ -72,7 +73,7 @@ fn interpret_round_p1(inr: &RoundInput) -> Round {
     (inr.0, self_move)
 }
 
-fn interpret_round_p2(inr: &RoundInput) -> Round {
+const fn interpret_round_p2(inr: &RoundInput) -> Round {
     let self_move = match inr {
         (o, SecondColumn::X) => which_loses(*o),
         (o, SecondColumn::Y) => *o,
