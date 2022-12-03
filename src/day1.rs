2252ec377beac777e3a1984c64d892b5fd9d5a2c
proegssilb <proegssilb@gmail.com>
2022-12-03 00:41:02 +0000

    Optimize Day 1 code

--- src/day1.rs
+++ src/day1.rs
@@ -1,38 +1,39 @@
+use std::collections::BinaryHeap;
 use aoc_runner_derive::{aoc_generator, aoc};
-use itertools::Itertools;
 
 #[aoc_generator(day1)]
-pub fn input_generator(input: &str) -> Vec<u64> {
-    input
-        .lines()
-        .map(|line| {
-            match line.trim() {
-                "" => None,
-                _ => Some(line.trim().parse::<u64>().unwrap()),
-            }
-        })
-        .group_by(|x| x.is_some())
-        .into_iter()
-        .filter(|(key, _)| *key )
-        .map(|(_, group)| {
-            let items : Vec<u64> = group.map(|i| i.unwrap()).collect();
-            items.iter().sum()
-        }).collect()
+pub fn input_generator(input: &str) -> Vec<i32> {
+    let mut results: Vec<i32> = Vec::new();
+    let mut current_sum: i32 = 0;
+    for line in input.lines() {
+        let trimmed = line.trim();
+        if trimmed.is_empty() {
+            results.push(current_sum);
+            current_sum = 0;
+        } else {
+            current_sum += trimmed.parse::<i32>().unwrap();
+        }
+    }
+    results
 }
 
 #[aoc(day1, part1)]
-pub fn solve_part1(input: &Vec<u64>) -> u64 {
-    (*input)
-        .clone()
-        .into_iter()
+pub fn solve_part1(input: &[i32]) -> i32 {
+    *input
+        .iter()
         .max()
-        .unwrap_or_default()
+        .unwrap()
 }
 
 #[aoc(day1, part2)]
-pub fn solve_part2(input: &Vec<u64>) -> u64 {
-    let mut max_backpacks = (*input)
-        .clone();
-    max_backpacks.sort_by_key(|i| -(*i as i64));
-    max_backpacks[0..3].iter().sum()
+pub fn solve_part2(input: &Vec<i32>) -> i32 {
+    let mut results = BinaryHeap::new();
+    for backpack in input {
+        results.push(backpack);
+        if results.len() > 3 {
+            _ = results.pop();
+        }
+    }
+
+    results.into_iter().sum()
 }
