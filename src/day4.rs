dea456be7374869b6ae8a4046d8108fee5d100f6
proegssilb <proegssilb@gmail.com>
2022-12-04 19:01:27 +0000

    Optimize/clean up Day 4

--- src/day4.rs
+++ src/day4.rs
@@ -1,46 +1,49 @@
-use std::ops::RangeInclusive;
+use std::cmp::Ordering;
 
 use aoc_runner_derive::{aoc, aoc_generator};
 use itertools::Itertools;
 
-type CampRange = RangeInclusive<i32>;
+type DataLine = (u8, u8, u8, u8);
 
 #[aoc_generator(day4)]
-pub fn input_generator(input: &str) -> Vec<(CampRange, CampRange)> {
-    input.lines()
-        .map(|l| {
-            let (a, b, c, d) = l.split(&[',', '-'])
-                .map(|s| s.parse().unwrap())
-                .collect_tuple()
-                .unwrap();
-            (CampRange::new(a, b), CampRange::new(c, d))
-        })
-        .collect_vec()
+pub fn input_generator(input: &str) -> Vec<DataLine> {
+    let mut results: Vec<DataLine> = Vec::new();
+    for line in input.lines() {
+        let (s1, rest) = line.split_once('-').unwrap();
+        let (e1, rest) = rest.split_once(',').unwrap();
+        let (s2, e2) = rest.split_once('-').unwrap();
+        results.push((s1.parse().unwrap(), e1.parse().unwrap(), s2.parse().unwrap(), e2.parse().unwrap()))
+    }
+    results
 }
 
-fn range_either_contains(a: &CampRange, b: &CampRange) -> i32 {
-    let res = (a.contains(b.start()) && a.contains(b.end())) || (b.contains(a.start()) && b.contains(a.end()));
+fn range_either_contains(dl: &DataLine) -> i32 {
+    let (a_s, b_s, a_e, b_e) = dl;
+    let cmp1 = a_s.cmp(b_s);
+    let cmp2 = a_e.cmp(b_e);
+    let res = cmp1 != cmp2 || cmp1 == Ordering::Equal;
     res as i32
 }
 
-fn range_overlaps(a: &CampRange, b: &CampRange) -> i32 {
-    let res = !(a.end() < b.start() || b.end() < a.start());
+fn range_overlaps(dl: &DataLine) -> i32 {
+    let (a_s, b_s, a_e, b_e) = dl;
+    let res = !(a_e < b_s || b_e < a_s);
     res as i32
 }
 
 #[aoc(day4, part1)]
-pub fn solve_part1(input: &[(CampRange, CampRange)]) -> i32 {
+pub fn solve_part1(input: &[DataLine]) -> i32 {
     input
         .iter()
-        .map(|(a, b)|range_either_contains(a, b))
+        .map(range_either_contains)
         .sum()
 }
 
 #[aoc(day4, part2)]
-pub fn solve_part2(input: &[(CampRange, CampRange)]) -> i32 {
+pub fn solve_part2(input: &[DataLine]) -> i32 {
     input
         .iter()
-        .map(|(a, b)|range_overlaps(a, b))
+        .map(range_overlaps)
         .sum()
 }
 
